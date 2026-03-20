44faae76a2537fa8477f44d127d628347bac8527
Mohammed Tariq R <mohammedtariq3006@gmail.com>
2026-03-20 15:55:15 +0530

    Add files via upload

---
+++ python-forloop.py
@@ -0,0 +1,26 @@
+
+
+#for loop 
+
+
+for i in range(1,10):
+    print("python-pratice")
+
+
+
+
+for i in range(1,21):
+    if(i%2==0):
+        print(i)
+
+
+
+
+
+#sum of loop
+
+sum=0
+for i in range(1,11):
+    if i%2==0:
+        sum=sum+i
+print(sum)
