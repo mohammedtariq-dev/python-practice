d2e1ed2cc877064b3a74755e35b6ff9e639818c3
Mohammed Tariq R <mohammedtariq3006@gmail.com>
2026-03-21 22:36:27 +0530

    Add files via upload

---
+++ python-whileloop.py
@@ -0,0 +1,20 @@
+n = int(input("Enter number: "))
+i = 1
+
+while i <= n:
+    print(i)
+    i += 1
+
+
+
+
+#sum of number 
+    n = int(input("Enter number: "))
+i = 1
+total = 0
+
+while i <= n:
+    total += i
+    i += 1
+
+print("Sum:", total)
